0a8f3435aea00525f7f9423fbd78571d3168bfe2
Alejandro Alcalde <algui91@gmail.com>
2012-08-31 12:35:21 +0200

    Write EOF byte using write_bit_by_bit() function for optimization

--- src/codificar.cpp
+++ src/codificar.cpp
@@ -11,8 +11,8 @@
 
 using namespace std;
 
-const bool WRITE_FILE_DATA = true;
-const bool WRITE_FILE_NAME = false;
+const bool WRITE_FROM_FILE = true;
+const bool WRITE_FROM_ARRAY = false;
 
 //Calcula el tamaño en bytes del fichero
 int get_file_size(ifstream& f){
@@ -22,8 +22,8 @@ int get_file_size(ifstream& f){
 
 	return size;
 }
-
-int write_bit_by_bit(unsigned char buffer[], ifstream& f,int from, int to, char sms[], bool type){
+//TODO hacer que ifstream sea opcional
+int write_bit_by_bit(unsigned char buffer[], ifstream& f, int from, int to, char sms[], bool type){
 
 	unsigned short int indiceLetra		  = 0;
 	unsigned char mask					  = 0x80; //Empezamos por el bit más significativo (10000000)
@@ -31,7 +31,7 @@ int write_bit_by_bit(unsigned char buffer[], ifstream& f,int from, int to, char 
 	char* file_buffer = 0;
 
 	if(type){ //Write file data
-		int number_of_bytes_to_read = to;//get_file_size(f); //TODO, se lo paso como parametro, no calcular de nuevo
+		int number_of_bytes_to_read = to;//get_file_size(f);
 		file_buffer = new char[number_of_bytes_to_read];
 		f.read(file_buffer, number_of_bytes_to_read);
 	}
@@ -40,7 +40,7 @@ int write_bit_by_bit(unsigned char buffer[], ifstream& f,int from, int to, char 
 	char letra = place_to_get_stuff_from[0];
 	int indice = from;
 
-	for (int i = 0; i < to; i++) { //TODO antes <=
+	for (int i = 0; i < to; i++) {
 		for (int k = 7; k >= 0; k--){
 			char c = (letra & mask) >> k;
 			mask >>= 1;
@@ -74,7 +74,7 @@ int ocultar(unsigned char buffer[],int tamImage, char sms[], int tamSms){
 		buffer[fin_cabecera] = 0xff;
 
 		//Escribo el nombre del archivo a ocultar
-		write_bit_by_bit(buffer, f, 1, strlen(sms), sms, WRITE_FILE_NAME);//TODO el 1
+		write_bit_by_bit(buffer, f, 1, strlen(sms), sms, WRITE_FROM_ARRAY);
 
 		int tamanio_en_bytes = get_file_size(f) /** 8*/;
 
@@ -82,21 +82,13 @@ int ocultar(unsigned char buffer[],int tamImage, char sms[], int tamSms){
 		// el for acaba cuando escribe todos los caracteres del fichero, hay que calcular
 		// el indice sumando el desplazamiento que ya acarreamos + los bytes del archivo pasados a bits
 		int datos_fichero = fin_cabecera + 1;
-		int ind = write_bit_by_bit(buffer, f, datos_fichero, tamanio_en_bytes/*tamanio_en_bytes + datos_fichero*/, sms, WRITE_FILE_DATA);
+		int ind = write_bit_by_bit(buffer, f, datos_fichero, tamanio_en_bytes, sms, WRITE_FROM_FILE);
 
 		//Escribo 0xff para indicar EOF de los datos
-		unsigned char fin_contenido = 0x7f;
+		char eof = 0x7f;
+		char* fin_contenido = &eof;
 
-		short int bitsLetraRestantes 		  = 7;
-		unsigned char mask					  = 0x80; //Empezamos por el bit más significativo (10000000)
-
-		for (int i = ind;	i < ind+8; i++) {
-			buffer[i] &= 0xfe;
-			char c = (fin_contenido & mask) >> bitsLetraRestantes--;
-			mask >>= 1;
-			buffer[i] ^= c; //Almacenamos en el ultimo bit del pixel el valor del caracter
-		}
-		cout << "FIN: " << tamanio_en_bytes + datos_fichero + 1;
+		write_bit_by_bit(buffer, f, ind, 1, fin_contenido, WRITE_FROM_ARRAY);
 	}
 	return 0;
 }
